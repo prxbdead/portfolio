4ccc1574b36c21945fcdc16e7a8563e5ce02fdeb
prxbdead <roland.gerg.123@gmail.com>
2024-07-20 05:54:28 +0300

    some basic taskbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref, onMounted, onUnmounted } from 'vue'
+
 import HelloWorld from './components/HelloWorld.vue'
+
+const startMenu = ref('')
+const currentTime = ref(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }))
+const apps = ref([
+  { name: 'teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeest' },
+  { name: 'asdfaaaaaaaaaafasfadafsafasfa' },
+  { name: '3' },
+  { name: '4' },
+  { name: '5' },
+  { name: '6' }
+])
+
+const activeApp = ref(apps.value[0])
+
+const updateCurrentTime = () => {
+  currentTime.value = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+}
+
+let intervalId: number | undefined
+
+onMounted(() => {
+  intervalId = setInterval(updateCurrentTime, 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId)
+})
 </script>
 
 <template>
+  <!--
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
@@ -17,69 +47,35 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView />-->
+  <footer>
+    <div class="container">
+      <div id="left">
+        <input type="checkbox" id="startMenu" v-model="startMenu" />
+        <label class="btn btn-startmenu" :class="{ active: startMenu }" for="startMenu">
+          <span><img src="./assets/icons/startmenu.png" draggable="false" /></span>
+          <span>Start</span>
+        </label>
+      </div>
+      <div id="center">
+        <div
+          class="btn btn-open"
+          :class="{ active: app == activeApp && !startMenu }"
+          :style="{ width: 'calc((100vw - 174px) / ' + apps.length + ')', 'max-width': '10vw' }"
+          v-for="app in apps"
+          @click="(activeApp = app), (startMenu = '')"
+        >
+          <img src="./assets/icons/startmenu.png" draggable="false" />
+          <span>
+            {{ app.name }}
+          </span>
+        </div>
+      </div>
+      <div id="right">
+        <div class="btn btn-time">
+          <span> {{ currentTime }} </span>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
