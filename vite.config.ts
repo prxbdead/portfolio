87aa8c2801d41e7ea366d88f9481885dd2cd75e0
prxbdead <roland.gerg.123@gmail.com>
2024-07-21 00:02:36 +0300

    fixed config for cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/portofolio/' : '/',
+  base: 'https://prxb.hu/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
